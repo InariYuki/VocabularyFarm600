403c220c626175fe4af3b106aa63672832f7f0d2
benforworkuse <105775320+benforworkuse@users.noreply.github.com>
2022-10-17 23:13:17 +0800

    +++

--- VocabularyFarm600/Assets/System/ScreenShotManager.cs
+++ VocabularyFarm600/Assets/System/ScreenShotManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ScreenShotManager : MonoBehaviour
 {
 
-    private void ScreenShot()
+    public void ScreenShot()
     {
         string path=Application.streamingAssetsPath;
         ScreenCapture.CaptureScreenshot(path+$"/{Random.Range(1000,100000)}.png");
